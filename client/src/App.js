84b67696be363dd6eda35ea74a3b75a6410f9af9
javenkn <javenkn7@yahoo.com>
2019-08-02 21:00:14 -1000

    Initial setup for the front end side

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
+
+import './App.css';
+
+function App() {
+  useEffect(() => {
+    const socket = socketIOClient('http://127.0.0.1:4001');
+    socket.on('connect_error', () => socket.close());
+    socket.on('FromAPI', data => console.log(data));
+    socket.on('disconnect', () => socket.close());
+    return () => {
+      socket.close();
+    };
+  }, []);
+  return <div className='App'>Hello</div>;
+}
+
+export default App;
