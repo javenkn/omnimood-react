a0bd707e387b9b6fe7d1b556accaadb03847366b
javenkn <javenkn7@yahoo.com>
2019-08-16 20:19:05 -1000

    adds countryCode to data formatter

--- server/helpers/formatData.js
+++ server/helpers/formatData.js
@@ -4,6 +4,7 @@ const formatData = data => {
   const { coordinates } = data.place.bounding_box;
   return {
     coordinateData: { coordinates: coordinates[0][0] },
+    countryCode: data.place.country_code,
     emoji: parseEmoji(data.text),
   };
 };
